92893dc272476303f788e75222524fa8496ce1d6
zisansarac <saraczisan02@gmail.com>
2025-04-24 17:03:13 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     
        <>
          <Hero />
+         <ShowcaseSection />
        
        
        </>
